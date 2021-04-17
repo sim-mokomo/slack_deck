a38a1eccc65025b2a635a0a6b399157744d5febe
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-04-17 22:26:41 +0900

    impl: webview表示を行えるように

--- converted_src/main.js
+++ converted_src/main.js
@@ -4,27 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
+const electron_2 = require("electron");
+const electron_3 = require("electron");
 const path_1 = __importDefault(require("path"));
 function createWindow() {
     const window = new electron_1.BrowserWindow({
-        width: 800,
-        height: 600,
         webPreferences: {
             preload: path_1.default.join(__dirname, 'preload.js')
         }
     });
-    window.loadFile('src/index.html');
+    window.maximize();
+    const webView = new electron_3.BrowserView();
+    window.setBrowserView(webView);
+    const winodwBounds = window.getBounds();
+    webView.setAutoResize({ width: true, height: true, horizontal: true, vertical: true });
+    webView.setBounds({ x: 0, y: 0, width: winodwBounds.width, height: winodwBounds.height });
+    webView.webContents.loadURL('https://www.google.com/');
 }
-electron_1.app.whenReady().then(() => {
+electron_2.app.whenReady().then(() => {
     createWindow();
-    electron_1.app.on('activate', () => {
+    electron_2.app.on('activate', () => {
         if (electron_1.BrowserWindow.getAllWindows().length == 0) {
             createWindow();
         }
     });
 });
-electron_1.app.on('window-all-closed', () => {
+electron_2.app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        electron_1.app.quit();
+        electron_2.app.quit();
     }
 });
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import {app, BrowserWindow} from "electron"
+import { BrowserWindow } from "electron"
+import { app } from "electron"
+import { BrowserView } from 'electron'
 import path from 'path'
 
 function createWindow() {
@@ -9,7 +11,12 @@ function createWindow() {
     })
     window.maximize()
 
-    window.loadFile('src/index.html')
+    const webView = new BrowserView()
+    window.setBrowserView(webView)
+    const winodwBounds = window.getBounds()
+    webView.setAutoResize({width: true, height: true, horizontal: true, vertical:true})
+    webView.setBounds({x: 0, y: 0, width: winodwBounds.width, height: winodwBounds.height})
+    webView.webContents.loadURL('https://www.google.com/')
 }
 
 app.whenReady().then(()=>{
