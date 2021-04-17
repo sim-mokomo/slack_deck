dda9210bb741dd243d88ac25dbbaa96d43b3b228
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-04-17 20:29:14 +0900

    impl: add quick start

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import {app, BrowserWindow} from "electron"
+import path from 'path'
+
+function createWindow() {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    window.loadFile('src/index.html')
+}
+
+app.whenReady().then(()=>{
+    createWindow()
+
+    app.on('activate', ()=>{
+        if(BrowserWindow.getAllWindows().length == 0){
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
