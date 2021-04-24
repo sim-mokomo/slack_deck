3f6ad0c35f365e9d1bfadfb9f9b1096ac7509e03
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-04-24 17:26:04 +0900

    impl: webview表示用のurlを構成できるように

--- converted_src/slack-service.js
+++ converted_src/slack-service.js
@@ -7,5 +7,19 @@ class SlackService {
     static getWorkspaceUrl(workspaceId = "") {
         return `https://app.slack.com/client/${workspaceId}`;
     }
+    static getChannelUrl(workspaceId, channelId) {
+        return `${this.getWorkspaceUrl(workspaceId)}/${channelId}`;
+    }
+    static getThreadUrl(workspaceId, channelId, threadTs) {
+        return `${this.getChannelUrl(workspaceId, channelId)}/thread/${this.createThreadId(channelId, threadTs)}`;
+    }
+    static createThreadId(channelId, threadTs) {
+        return `${channelId}-${threadTs}`;
+    }
+    static getWebViewURL(workspaceId, channelId, threadId) {
+        return threadId.length > 0 ?
+            this.getThreadUrl(workspaceId, channelId, threadId) :
+            this.getChannelUrl(workspaceId, channelId);
+    }
 }
 exports.SlackService = SlackService;
--- src/slack-service.ts
+++ src/slack-service.ts
@@ -4,4 +4,22 @@ export class SlackService {
     static getWorkspaceUrl(workspaceId = "") : string{
         return `https://app.slack.com/client/${workspaceId}`
     }
+
+    static getChannelUrl(workspaceId:string, channelId:string) : string {
+        return `${this.getWorkspaceUrl(workspaceId)}/${channelId}`
+    }
+
+    static getThreadUrl(workspaceId:string, channelId:string, threadTs:string) :string{
+        return `${this.getChannelUrl(workspaceId,channelId)}/thread/${this.createThreadId(channelId,threadTs)}`
+    }
+
+    static createThreadId(channelId:string, threadTs:string) : string{
+        return `${channelId}-${threadTs}`
+    }
+
+    static getWebViewURL(workspaceId:string , channelId: string, threadId:string) : string {
+        return threadId.length > 0 ?
+            this.getThreadUrl(workspaceId, channelId, threadId) :
+            this.getChannelUrl(workspaceId, channelId)
+    }
 }
