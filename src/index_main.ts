75d008ca6ae6ab899c023adcf48676168bc7c321
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-04-30 23:42:18 +0900

    impl: カラム追加用のリクエスト作成

--- src/index_main.ts
+++ src/index_main.ts
@@ -1,4 +1,5 @@
-import {ipcMain} from "electron";
+import {ipcMain, shell} from "electron";
+import {IpcMainEvent} from "electron"
 import {AppConfig, WorkSpaceColumnConfig} from "./config";
 import {SlackService} from "./slack-service";
 
@@ -11,24 +12,12 @@ export class IndexMainProcess{
                 return
             }
 
-            const launcherUrls : string[] = []
-            for (const columnConfig of workspaceConfig.columns) {
-                const url = columnConfig.thread_ts.length > 0 ?
-                    SlackService.getThreadUrl(
-                        workspaceConfig.workspace_id,
-                        columnConfig.channel_id,
-                        columnConfig.thread_ts
-                    )
-                    :
-                    SlackService.getChannelUrl(
-                        workspaceConfig.workspace_id,
-                        columnConfig.channel_id
-                    )
-                launcherUrls.push(url)
-                console.log(url)
-            }
-
-            event.sender.send('add-slack-column-reply',launcherUrls)
+            const requests : AddSlackColumnResponse[] =
+                workspaceConfig.columns.map((x,index) => new AddSlackColumnResponse(
+                    SlackService.getWebViewURL(workspaceConfig.workspace_id, x.channel_id, x.thread_ts),
+                    index
+                ))
+            this.addSlackColumnResponse(event, requests)
         })
 
         ipcMain.on('add-column-main-request', (event, arg) => {
@@ -41,9 +30,25 @@ export class IndexMainProcess{
             appConfig.addWorkSpaceColumnConfig(workSpaceConfig.workspace_id, newColumn)
             AppConfig.save(appConfig)
 
-            const columnUrl = SlackService.getWebViewURL(workSpaceConfig.workspace_id, channelId, threadTs)
-            event.sender.send('add-slack-column-reply', [columnUrl])
+            const request : AddSlackColumnResponse = {
+                url: SlackService.getWebViewURL(workSpaceConfig.workspace_id, channelId, threadTs),
+                id: workSpaceConfig.columns.length
+            }
+            this.addSlackColumnResponse(event, [request])
         })
+    }
+
+    addSlackColumnResponse(event: IpcMainEvent, requests:AddSlackColumnResponse[]) : void {
+        event.sender.send('add-slack-column-reply', JSON.stringify(requests))
     }
 }
 
+export class AddSlackColumnResponse {
+    url : string
+    id: number
+
+    constructor(url:string, id:number) {
+        this.url = url
+        this.id = id
+    }
+}
