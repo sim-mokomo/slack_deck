dd0f014cff5fd790c21b0152c393a2abb7d1169c
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-07-10 23:26:07 +0900

    fix rename: config -> app-config

---
+++ src/app-config.ts
@@ -0,0 +1,60 @@
+import fs from "fs"
+import {WorkspaceConfig} from "./workspace-config";
+import {WorkspaceColumnConfig} from "./workspace-column-config";
+
+export class AppConfig {
+	workspaces: WorkspaceConfig[] = []
+	static readonly fileName = "appconfig.json"
+
+	static load(): AppConfig {
+		if (!fs.existsSync(this.fileName)) {
+			return this.save(this.getEmpty())
+		}
+		const ret: AppConfig = new AppConfig()
+		Object.assign(ret, JSON.parse(fs.readFileSync(this.fileName, "utf-8")))
+		return ret
+	}
+
+	static save(config: AppConfig): AppConfig {
+		fs.writeFileSync(this.fileName, JSON.stringify(config,null , "\t"))
+		return config
+	}
+
+	static getEmpty(): AppConfig {
+		const config = new AppConfig()
+		const workspaceConfig = new WorkspaceConfig()
+		workspaceConfig.addColumn(WorkspaceColumnConfig.getEmpty())
+		config.workspaces.push(workspaceConfig)
+		return config
+	}
+
+	addWorkspaceColumnConfig(workspaceId: string, workspaceColumnConfig: WorkspaceColumnConfig): void {
+		const workspaceConfig = this.getWorkspaceConfig(workspaceId)
+		if(workspaceConfig != null){
+			workspaceConfig.addColumn(workspaceColumnConfig)
+		}
+	}
+
+	removeWorkspaceColumnConfig(workspaceId: string, columnId: number): void {
+		const workspaceConfig = this.getWorkspaceConfig(workspaceId)
+		if(workspaceConfig != null) {
+			workspaceConfig.removeColumn(columnId)
+		}
+	}
+
+	getWorkspaceConfig(workspaceId:string) : WorkspaceConfig | null {
+		const index = this.workspaces.findIndex(x => x.workspace_id == workspaceId)
+		if(index < 0 || index >= this.workspaces.length){
+			return null
+		}
+
+		return this.workspaces[index]
+	}
+
+	getWorkspaceConfigHead() : WorkspaceConfig | null {
+		return this.workspaces[0]
+	}
+
+}
+
+
